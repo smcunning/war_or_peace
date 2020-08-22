0998b1bc493afe8738937da80302de7d69f83051
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-21 19:10:12 -0600

    Add test for welcome message

--- test/game_test.rb
+++ test/game_test.rb
@@ -9,10 +9,40 @@ require 'minitest/pride'
 class GameTest < MiniTest::Test
 
   def test_it_exists
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
 
     game = Game.new
 
     assert_instance_of Game, game
   end
 
+  def test_it_has_welcome_message
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    game = Game.new
+
+    assert_equal "Welcome to War! (or Peace) This game will be played with 52 cards.", game.welcome_message
+  end
 end
