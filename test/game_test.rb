5312d6fa75dfef49744ffb07a8215ebeadf6d363
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-23 16:39:45 -0600

    Add tests for players, turn count and turn messages

--- test/game_test.rb
+++ test/game_test.rb
@@ -7,69 +7,8 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class GameTest < MiniTest::Test
-  #
-  # def test_it_exists
-  #   card1 = Card.new(:heart, 'Jack', 11)
-  #   card2 = Card.new(:heart, '10', 10)
-  #   card3 = Card.new(:heart, '9', 9)
-  #   card4 = Card.new(:diamond, 'Jack', 11)
-  #   card5 = Card.new(:heart, '8', 8)
-  #   card6 = Card.new(:diamond, 'Queen', 12)
-  #   card7 = Card.new(:heart, '3', 3)
-  #   card8 = Card.new(:diamond, '2', 2)
-  #   deck1 = Deck.new([card1, card2, card5, card8])
-  #   deck2 = Deck.new([card3, card4, card6, card7])
-  #   player1 = Player.new("Megan", deck1)
-  #   player2 = Player.new("Aurora", deck2)
-  #
-  #   game = Game.new
-  #
-  #   assert_instance_of Game, game
-  # end
-  #
-  # def test_it_has_welcome_message
-  #   card1 = Card.new(:heart, 'Jack', 11)
-  #   card2 = Card.new(:heart, '10', 10)
-  #   card3 = Card.new(:heart, '9', 9)
-  #   card4 = Card.new(:diamond, 'Jack', 11)
-  #   card5 = Card.new(:heart, '8', 8)
-  #   card6 = Card.new(:diamond, 'Queen', 12)
-  #   card7 = Card.new(:heart, '3', 3)
-  #   card8 = Card.new(:diamond, '2', 2)
-  #   deck1 = Deck.new([card1, card2, card5, card8])
-  #   deck2 = Deck.new([card3, card4, card6, card7])
-  #   player1 = Player.new("Megan", deck1)
-  #   player2 = Player.new("Aurora", deck2)
-  #
-  #   game = Game.new
-  #
-  #   assert_equal "Welcome to War!(or Peace) This game will be played with 52 cards.", game.welcome_message
-  # end
-  #
-  # def test_user_can_tell_it_to_go
-  #   card1 = Card.new(:heart, 'Jack', 11)
-  #   card2 = Card.new(:heart, '10', 10)
-  #   card3 = Card.new(:heart, '9', 9)
-  #   card4 = Card.new(:diamond, 'Jack', 11)
-  #   card5 = Card.new(:heart, '8', 8)
-  #   card6 = Card.new(:diamond, 'Queen', 12)
-  #   card7 = Card.new(:heart, '3', 3)
-  #   card8 = Card.new(:diamond, '2', 2)
-  #   deck1 = Deck.new([card1, card2, card5, card8])
-  #   deck2 = Deck.new([card3, card4, card6, card7])
-  #   player1 = Player.new("Megan", deck1)
-  #   player2 = Player.new("Aurora", deck2)
-  #
-  #   game = Game.new
-  #
-  #   #It works is a placeholder - this will eventually prompt the game to start
-  #   #assert_equal "it works!", game.welcome_message
-  #
-  #   #Test for something other than go
-  #   assert_equal "Try typing 'GO' to play!", game.start
-  # end
 
-  def test_game_can_start_and_run
+  def test_it_exists
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -83,10 +22,108 @@ class GameTest < MiniTest::Test
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
 
-    game = Game.new
+    game = Game.new(player1, player2)
 
-    assert_equal "Turn 1: Megan won 2 cards", game.start
+    assert_instance_of Game, game
+  end
 
+  def test_it_has_players
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    game = Game.new(player1, player2)
+
+    assert_equal player1, game.player1
+  end
+
+  def test_turn_count_starts_at_1
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    game = Game.new(player1, player2)
+
+    assert_equal 1, game.turn_count
+  end
+
+  def test_it_has_a_basic_turn_message
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    game = Game.new(player1, player2)
+    turn_count = game.turn_count + 1
+
+    assert_equal "Turn 1: Megan won 2 cards", game.turn_message(turn)
+  end
+
+
+  def test_it_has_a_war_turn_message
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    game = Game.new(player1, player2)
+    turn_count = game.turn_count + 1
+
+    assert_equal "Turn 1: WAR - Aurora won 6 cards", game.turn_message(turn)
+  end
+
+  def test_it_has_a_mad_turn_message
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    game = Game.new(player1, player2)
+    turn_count = game.turn_count + 1
+
+    assert_equal "Turn 1: *mutually assured destruction* 6 cards removed from play", game.turn_message(turn)
   end
 
 end
